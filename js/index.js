b1064713c867b332c9be1b57574031133f4f62a0
mofe-oluneye <mofeoluneye@gmail.com>
2025-09-01 18:35:43 +0100

    work

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-function signUp(event){
+// function signUp(event){
 
-    event.preventDefault(); 
+//     event.preventDefault(); 
 
-    const spinItem = document.querySelector(".spin");
-    spinItem.style.display = "inline-block";
+//     const spinItem = document.querySelector(".spin");
+//     spinItem.style.display = "inline-block";
     
     
-      const getName = document.getElementById("name").value;
-    const getEmail = document.getElementById("email").value;
-    const getPhone = document.getElementById("phoneNumber").value;
-    const getPassword = document.getElementById("enter Password").value;  
-    // const getPassword = document.getElementById("defaultCheck1").value;  
-    // const Pass = document.getElementById("defaultCheck1");
+//       const getName = document.getElementById("name").value;
+//     const getEmail = document.getElementById("email").value;
+//     const getPhone = document.getElementById("phoneNumber").value;
+//     const getPassword = document.getElementById("enter Password").value;  
+//     // const getPassword = document.getElementById("defaultCheck1").value;  
+//     // const Pass = document.getElementById("defaultCheck1");
 
 
-    if (getName === "" || getEmail === "" || getPhone === "" || getPassword === "" ) {
-        Swal.fire({
-            icon: 'info',
-            text: 'All fields are required! including the privacy policy and terms of use',
-            confirmButtonColor: "#2D85DE"
-        })       
-         spinItem.style.display = "none";
-        return;
-          }
-} 
+//     if (getName === "" || getEmail === "" || getPhone === "" || getPassword === "" ) {
+//         Swal.fire({
+//             icon: 'info',
+//             text: 'All fields are required! including the privacy policy and terms of use',
+//             confirmButtonColor: "#2D85DE"
+//         })       
+//          spinItem.style.display = "none";
+//         return;
+//           }
+// } 
 
     
 
